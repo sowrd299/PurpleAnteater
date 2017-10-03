8878b07ac234b2668057f367815f0384f7419b26
sowrd299 <ryan.ward46@gmail.com>
2017-10-02 21:14:23 -0700

    index debug

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
-	redirect("home.php");
+    //header('Location: http://clubs.uci.edu/vgdc/home.php');
+    header('Location: http://108.88.231.121/vgdc-uci/home.php'); //testing only
+    die();
 ?>
