b94c52ab45509613f3a16da029186e207de9e412
Video Game Development Club <vgdc@clubs.uci.edu>
2018-01-07 13:21:36 -0800

    updates meeting

--- meetings.php
+++ meetings.php
@@ -17,12 +17,15 @@
                 <h3>GENERAL MEETINGS</h3> 
                 <!--
                 <p>Every <b>Wednesday at 7:00 p.m.</b> in <b>Social Science Lab 228</b></p> 
-                -->
                 <p>Every <b>Tuesday at 8:30 p.m.</b> in <b>Social Science Lecture Hall 100</b></p> 
+                -->
+		<p>Meeting Time and Place TBA</b></p>
 
                 <div class = "left">
                     <!-- NOTE: Update BOTH bellow URL's for each new classroom -->
+		<!--
                 	<a href="http://www.classrooms.uci.edu/GAC/SSLH100.html"><img style="max-width:100%;" src="http://www.classrooms.uci.edu/gac/mapgrids/SSLH.png"></a>
+		-->
                 </div>
 
                 <div class = "left">
